632588b91bc3aadd9d2bc6845bcf09cdc5980143
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-01-11 21:51:00 +0100

    filter method has been added

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//7:19
+const brightnessBar = document.getElementById('brightness-bar');
+const contrastBar = document.getElementById('contrast-bar');
+const saturationBar = document.getElementById('saturation-bar');
+const hueBar = document.getElementById('hue-bar');
+const blurBar = document.getElementById('blur-bar');
+const image = document.querySelector('.image');
+
+const updatesFilters = () => {
+    image.style.filter = `brightness(${brightnessBar.value}%) 
+    contrast(${contrastBar.value}%) 
+    saturate(${saturationBar.value}%) 
+    hue-rotate(${hueBar.value}deg) 
+    blur(${blurBar.value}px)`;
+};
+
+[brightnessBar, contrastBar, saturationBar, hueBar, blurBar].forEach((bar) => {
+    bar.addEventListener('input', updatesFilters);
+})
+
+
